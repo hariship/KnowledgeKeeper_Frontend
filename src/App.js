8fe9c4bb2bba12dbdab33ac9ae42925effcbefd8
hp-qwlt <haripriya@qwlt.io>
2024-09-30 16:27:07 +0530

    fixed login path

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import {
   Navigate,
 } from "react-router-dom";
 
-import LoginPage from "./pages/LoginPage";
+import LoginPage from "./pages/loginPage";
 import HomePage from "./pages/homePage";
 import { AuthProvider } from "./components/ProtectedRoute/AuthContext";
 import { ToastContainer } from "react-toastify";
